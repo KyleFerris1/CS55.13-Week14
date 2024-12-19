edd87c5586befe01282f7416d73bc98d624eb26e
KyleFerris1 <kyleferrismv@gmail.com>
2024-12-19 10:10:40 +0000

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,17 @@
 import Link from 'next/link';
 import Layout from '../components/layout';
 import { getSortedList as getContacts } from '../lib/data';
-import { getSortedList as getProducts } from '../lib/data2';
-import { getSortedList as getLocations } from '../lib/data3';
+import { getSortedList as getProducts} from '../lib/data2';
+import { getSortedList as getLocations} from '../lib/data3';
 
 export async function getStaticProps() {
   const contacts = await getContacts();
   const products = await getProducts();
   const locations = await getLocations();
   
-  return {
-    props: {
+  
+  return{
+    props:{
       contacts,
       products,
       locations
@@ -19,34 +20,46 @@ export async function getStaticProps() {
   };
 }
 
-export default function Home({ contacts, products, locations }) {
+
+export default function Home({contacts, products, locations}) {
   return (
     <Layout home>
       <h1>Contacts</h1>
       <div className="list-group">
-        {contacts.map(({id, name}) => (
-          <Link key={id} href={`/${id}`} className="list-group-item list-group-item-action">
-            {name}
-          </Link>
-        ))}
+        {contacts.map(
+          ({id, name}) => (
+            <Link key={id} href={`/${id}`} className="list-group-item list-group-item-action">
+              {name}
+            </Link>
+          )
+        )
+        }
       </div>
+
+      
 
       <h1>Products</h1>
       <div className="list-group">
-        {products.map(({id, name}) => (
-          <Link key={id} href={`/${id}`} className="list-group-item list-group-item-action">
-            {name}
-          </Link>
-        ))}
+        {products.map(
+          ({id, name}) => (
+              <Link key={id} href={`/${id}`} className="list-group-item list-group-item-action">
+                {name}
+              </Link>
+            )
+          )
+          }
       </div>
 
       <h1>Locations</h1>
       <div className="list-group">
-        {locations.map(({id, name}) => (
-          <Link key={id} href={`/${id}`} className="list-group-item list-group-item-action">
-            {name}
-          </Link>
-        ))}
+        {locations.map(
+          ({id, name}) => (
+            <Link key={id} href={`/${id}`} className="list-group-item list-group-item-action">
+              {name}
+            </Link>
+          )
+        )
+        }
       </div>
     </Layout>
   );
